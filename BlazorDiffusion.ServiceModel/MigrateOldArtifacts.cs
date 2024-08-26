b9ac47579e0f05d387e262981806c0fed08de850
Demis Bellot <demis.bellot@gmail.com>
2024-08-26 17:13:11 +0800

    Add MigrateOldArtifacts API

---
+++ BlazorDiffusion.ServiceModel/MigrateOldArtifacts.cs
@@ -0,0 +1,15 @@
+   using System.Collections.Generic;
+using ServiceStack;
+
+namespace BlazorDiffusion.ServiceModel;
+
+public class MigrateOldArtifacts : IGet, IReturn<MigrateOldArtifactsResponse>
+{
+    public int? Limit { get; set; }
+}
+public class MigrateOldArtifactsResponse
+{
+    public List<string> Results { get; set; } = [];
+    public List<string> Failed { get; set; } = [];
+    public ResponseStatus? ResponseStatus { get; set; }
+}
